0d8be06b3184fc3fca1e20fdd764be7dad2e960c
unknown <wdavies@ABAWP060.office.abanes.org>
2015-11-02 14:30:42 -0500

    WD - Ignored a compiled dll that should have been ignored via the gitignore file but may have already been under source control in the original repo. Dropped the xml template.  That should be addressed in the SDKs configuration (ie webconfig or app.settings).

--- FuelSDK-CSharp/JsonWebToken.cs
+++ FuelSDK-CSharp/JsonWebToken.cs
@@ -7,31 +7,6 @@ using Newtonsoft.Json.Linq;
 
 namespace FuelSDK
 {
-    /// <summary>
-    /// JWT for .NET
-    /// See https://github.com/johnsheehan/jwt
-    /// This is commit f4b6d0ce78
-    //Written by John Sheehan
-    //(http://john-sheehan.com)
-
-    //This work is public domain.
-    //"The person who associated a work with this document has 
-    // dedicated the work to the Commons by waiving all of his
-    // or her rights to the work worldwide under copyright law
-    // and all related or neighboring legal rights he or she
-    // had in the work, to the extent allowable by law."
-
-    //For more information, please visit:
-    //http://creativecommons.org/publicdomain/zero/1.0/
-
-    /// </summary>
-    public enum JwtHashAlgorithm
-    {
-        HS256,
-        HS384,
-        HS512
-    }
-
     public class JsonWebToken
     {
         private static Dictionary<JwtHashAlgorithm, Func<byte[], byte[], byte[]>> HashAlgorithms;
